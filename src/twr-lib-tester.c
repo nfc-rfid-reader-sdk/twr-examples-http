97170500e11f1688eb21ea74b030e1ba98ff65e9
developer3 <support@d-logic.rs>
2018-02-08 15:24:01 +0100

    Use hex2str() to create UID in hex-c-string format.

--- src/twr-lib-tester.c
+++ src/twr-lib-tester.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "tools.h"
 //-----------------------------
 // OS specific
 #include <windows.h>
@@ -22,19 +23,24 @@ static int polling = TRUE;
 // UID for stop polling - exit from application
 u8 stop_uid[4] = { 0x16, 0x78, 0x58, 0x56 };
 
+//------------------------------------------------------------------
+
+#define UID_STR_LEN_MAX		32
+
 int fCB_UID(c_string sn, u8 uid[], int uid_len, int control_info)
 {
 	TWR_STATUS e;
-	int i;
+	char uid_str[UID_STR_LEN_MAX + 1]; // +1 null character
+	size_t uid_str_len = UID_STR_LEN_MAX;
+
+	hex2str(uid, uid_len, uid_str, &uid_str_len);
+	// TODO: test hex2str() result
 
 	puts("");
-	puts("Application CB_OK is called ...");
+	puts("Application CB_UID is called ...");
 
-	printf("Arrived packet from TWR SN=[%8s] with UID[len: %d]=", sn, uid_len);
-	// hex to string
-	for (i = 0; i < uid_len; ++i)
-		printf("%02X:", uid[i]);
-	printf(" | control_info= %d\n\n", control_info);
+	printf("Arrived packet from TWR SN:[%8s] with UID[len:%d]= %s | "
+			"control_info= %d\n\n", sn, uid_len, uid_str, control_info);
 
 	puts("Application send ACK...");
 
